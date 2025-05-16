06542d887a95a36be203cde9a14ccecb5128c3cb
kohi9noor <kohinoornimes.dev@gmail.com>
2025-05-16 20:20:03 +0530

    add: api types axios client mutation fn

---
+++ app/src/lib/api.ts
@@ -0,0 +1,226 @@
+import API from "./axios-client";
+
+import {
+  CreateWorkspaceResponseType,
+  CreateWorkspaceType,
+  CurrentUserResponseType,
+  LoginResponseType,
+  loginType,
+  registerType,
+  EditWorkspaceType,
+  AllWorkspaceResposneType,
+  WorkspacebyIdResponseType,
+  AllMembersInWorkspaceResponseType,
+  AnalyticsResponseType,
+  ChangeWorkspaceMemberRoleType,
+  CreateProjectPayloadType,
+  ProjectResponseType,
+  AllProjectPayloadType,
+  AllProjectResponseType,
+  EditProjectPayloadType,
+  ProjectByIdPayloadType,
+  CreateTaskPayloadType,
+  AllTaskPayloadType,
+  AllTaskResponseType,
+} from "@/types/api.type";
+
+export const loginMutationFn = async (
+  data: loginType
+): Promise<LoginResponseType> => {
+  const response = await API.post("/auth/login", data);
+  return response.data;
+};
+
+export const registerMutationFn = async (data: registerType) =>
+  await API.post("/auth/register", data);
+
+export const logoutMutationFn = async () => await API.post("/auth/logout");
+
+export const getCurrentUserQueryFn =
+  async (): Promise<CurrentUserResponseType> => {
+    const response = await API.post("/user/current");
+    return response.data;
+  };
+
+export const createWorkspaceMutationFn = async (
+  data: CreateWorkspaceType
+): Promise<CreateWorkspaceResponseType> => {
+  const response = await API.post("/workspace/create/new", data);
+  return response.data;
+};
+
+export const editWorkspaceMutationFn = async ({
+  workspace_id,
+  data,
+}: EditWorkspaceType) => {
+  const response = await API.put(`/workspace/update/${workspace_id}`, data);
+  return response.data;
+};
+
+export const getAllWorkspaceUserIsMemberQueryFn =
+  async (): Promise<AllWorkspaceResposneType> => {
+    const response = await API.get("/workspace/all");
+    return response.data;
+  };
+
+export const getWorkspacebyIdQueryFn = async (
+  workspaceId: string
+): Promise<WorkspacebyIdResponseType> => {
+  const response = await API.get(`/workspace/${workspaceId}`);
+  return response.data;
+};
+
+export const getMembersInWorkspaceQueryFn = async (
+  workspaceId: string
+): Promise<AllMembersInWorkspaceResponseType> => {
+  const response = await API.get(`/workspace/members/${workspaceId}`);
+  return response.data;
+};
+
+export const getWorkspaceAnalyticsQUeryFn = async (
+  workspaceId: string
+): Promise<AnalyticsResponseType> => {
+  const response = await API.get(`/workspace/analytics/${workspaceId}`);
+  return response.data;
+};
+
+export const changeWorkspaceMemberRoleMutationFn = async ({
+  workspace_id,
+  data,
+}: ChangeWorkspaceMemberRoleType) => {
+  const response = await API.put(
+    `/workspace/change/member/role/${workspace_id}`,
+    data
+  );
+  return response.data;
+};
+
+export const deleteWorkspaceMutationFn = async (
+  workspaceId: string
+): Promise<{ message: string; current_workspace: string }> => {
+  const response = await API.delete(`/workspace/delete/${workspaceId}`);
+  return response.data;
+};
+
+export const invitedUserJoinWorkspaceMutationFn = async (
+  inviteCode: string
+): Promise<{ message: string; workspace_id: string }> => {
+  const response = await API.post(`/member/workspace/${inviteCode}/join`);
+  return response.data;
+};
+
+export const createProjectMutationFn = async ({
+  workspace_id,
+  data,
+}: CreateProjectPayloadType): Promise<ProjectResponseType> => {
+  const response = await API.post(
+    `/project/workspace/${workspace_id}/create`,
+    data
+  );
+  return response.data;
+};
+
+export const editProjectMutationFn = async ({
+  project_id,
+  workspace_id,
+  data,
+}: EditProjectPayloadType): Promise<ProjectResponseType> => {
+  const response = await API.put(
+    `/project/${project_id}/workspace/${workspace_id}/update`,
+    data
+  );
+  return response.data;
+};
+
+export const getProjectsInWorkspaceQueryFn = async ({
+  workspace_id,
+  page_size = 10,
+  page_number = 1,
+}: AllProjectPayloadType): Promise<AllProjectResponseType> => {
+  const response = await API.get(
+    `/project/workspace/${workspace_id}/all?pageSize=${page_size}&pageNumber=${page_number}`
+  );
+  return response.data;
+};
+
+export const getProjectByIdQueryFn = async ({
+  workspace_id,
+  project_id,
+}: ProjectByIdPayloadType): Promise<ProjectResponseType> => {
+  const response = await API.get(
+    `/project/${project_id}/workspace/${workspace_id}`
+  );
+  return response.data;
+};
+export const getProjectAnalyticsQueryFn = async ({
+  workspace_id,
+  project_id,
+}: ProjectByIdPayloadType): Promise<AnalyticsResponseType> => {
+  const response = await API.get(
+    `/project/${project_id}/workspace/${workspace_id}/analytics`
+  );
+  return response.data;
+};
+
+export const deleteProjectMutationFn = async ({
+  project_id,
+  workspace_id,
+}: ProjectByIdPayloadType): Promise<{ message: string }> => {
+  const response = await API.delete(
+    `/project/${project_id}/workspace/${workspace_id}/delete`
+  );
+
+  return response.data;
+};
+
+export const createTaskMutationFn = async ({
+  workspace_id,
+  project_id,
+  data,
+}: CreateTaskPayloadType) => {
+  const response = await API.post(
+    `/task/project/${project_id}/workspace/${workspace_id}/create`,
+    data
+  );
+  return response.data;
+};
+
+export const getAllTasksQueryFn = async ({
+  workspace_id,
+  keyword,
+  project_id,
+  assigned_to,
+  priority,
+  status,
+  dueDate,
+  page_number,
+  page_size,
+}: AllTaskPayloadType): Promise<AllTaskResponseType> => {
+  const baseURL = `/task/workspace/${workspace_id}/all`;
+
+  const queryParams = new URLSearchParams();
+  if (keyword) queryParams.append("keyword", keyword);
+  if (project_id) queryParams.append("project_id", project_id);
+  if (assigned_to) queryParams.append("assigned_to", assigned_to);
+  if (priority) queryParams.append("priority", priority);
+  if (status) queryParams.append("status", status);
+  if (dueDate) queryParams.append("dueDate", dueDate);
+  if (page_number) queryParams.append("page_number", page_number?.toString());
+  if (page_size) queryParams.append("page_size", page_size?.toString());
+  const url = queryParams.toString() ? `${baseURL}?${queryParams}` : baseURL;
+  const response = await API.get(url);
+  return response.data;
+};
+
+export const deleteTaskMutationFn = async ({
+  workspace_id,
+  task_id,
+}: {
+  workspace_id: string;
+  task_id: string;
+}) => {
+  const response = await API.delete(
+    `task/${task_id}/workspace/${workspace_id}/delete`
+  );
+  return response.data;
+};
---
+++ app/src/types/api.type.ts
@@ -0,0 +1,257 @@
+import {
+  PermissionsType,
+  TaskPriorityEnumType,
+  TaskStatusEnumType,
+} from "@/constant";
+
+export type loginType = { email: string; password: string };
+export type LoginResponseType = {
+  message: string;
+  user: {
+    user_id: string;
+    current_workspace: string;
+  };
+};
+
+export type registerType = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type UserType = {
+  user_id: string;
+  name: string;
+  email: string;
+  profile_picture: string | null;
+  is_active: true;
+  last_login: null;
+  created_at: Date;
+  updated_at: Date;
+  current_workspace: {
+    project_id: string;
+    name: string;
+    owner: string;
+    invite_code: string;
+  };
+};
+
+export type CurrentUserResponseType = {
+  message: string;
+  user: UserType;
+};
+
+export type WorkspaceType = {
+  workspace_id: string;
+  name: string;
+  description?: string;
+  owner: string;
+  invite_code: string;
+};
+
+export type CreateWorkspaceType = {
+  workspace_id: string;
+  data: {
+    name: string;
+    description: string;
+  };
+};
+
+export type CreateWorkspaceResponseType = {
+  message: string;
+  workspace: WorkspaceType;
+};
+export type AllWorkspaceResposneType = {
+  message: string;
+  workspaces: WorkspaceType[];
+};
+
+export type EditWorkspaceType = {
+  workspace_id: string;
+  data: {
+    name: string;
+    description: string;
+  };
+};
+export type WorkspaceWithMemberType = WorkspaceType & {
+  members: {
+    member_id: string;
+    user_id: string;
+    workspace_id: string;
+    role: {
+      id: string;
+      name: string;
+      permissions: PermissionsType[];
+    };
+  };
+  jointed_at: string;
+  created_at: string;
+};
+
+export type WorkspacebyIdResponseType = {
+  message: string;
+  workspace: WorkspaceWithMemberType[];
+};
+
+export type ChangeWorkspaceMemberRoleType = {
+  workspace_id: string;
+  data: {
+    role_id: string;
+    member_id: string;
+  };
+};
+
+export type AllMembersInWorkspaceResponseType = {
+  message: string;
+  members: {
+    member_id: string;
+    user: {
+      user_id: string;
+      name: string;
+      email: string;
+      profile_picture: string | null;
+    };
+  };
+  workspace_id: string;
+  role: {
+    id: string;
+    name: string;
+  };
+  joined_at: string;
+  created_at: string;
+  roles: RoleType[];
+};
+
+export type AnalyticsResponseType = {
+  message: string;
+  analytics: {
+    total_tasks: number;
+    overdue_tasks: number;
+    completed_tasks: number;
+  };
+};
+
+export type PaginationType = {
+  total_count: number;
+  page_size: number;
+  page_number: string;
+  total_pages: number;
+  skip: number;
+  limit: number;
+};
+
+export type RoleType = {
+  id: string;
+  name: string;
+};
+
+export type ProjectType = {
+  project_id: string;
+  name: string;
+  emoji: string;
+  description: string;
+  workspace: string;
+  created_by: {
+    user_id: string;
+    name: string;
+    profile_picture: string;
+  };
+  created_at: string;
+  updated_at: string;
+};
+
+export type CreateProjectPayloadType = {
+  workspace_id: string;
+  data: {
+    emoji: string;
+    name: string;
+    description: string;
+  };
+};
+
+export type ProjectResponseType = {
+  message: "Project created successfully";
+  project: ProjectType;
+};
+
+export type EditProjectPayloadType = {
+  workspace_id: string;
+  project_id: string;
+  data: {
+    emoji: string;
+    name: string;
+    description: string;
+  };
+};
+
+export type AllProjectPayloadType = {
+  workspace_id: string;
+  page_number?: number;
+  page_size: number;
+  keyword: string;
+  skip?: boolean;
+};
+
+export type AllProjectResponseType = {
+  message: string;
+  projects: ProjectType[];
+  pagination: PaginationType;
+};
+
+export type ProjectByIdPayloadType = {
+  workspace_id: string;
+  project_id: string;
+};
+
+export type CreateTaskPayloadType = {
+  workspace_id: string;
+  project_id: string;
+  data: {
+    title: string;
+    description: string;
+    priority: TaskPriorityEnumType;
+    status: TaskStatusEnumType;
+    assigned_to: string;
+    dueDate: string;
+  };
+};
+
+export type TaskType = {
+  task_id: string;
+  title: string;
+  description?: string;
+  project?: {
+    project_id: string;
+    emoji: string;
+    name: string;
+  };
+  priority: TaskPriorityEnumType;
+  status: TaskStatusEnumType;
+  assigned_to: {
+    user_id: string;
+    name: string;
+    profile_picture: string | null;
+  };
+  created_by?: string;
+  dueDate: string;
+  created_at?: string;
+  updated_at?: string;
+};
+
+export type AllTaskPayloadType = {
+  workspace_id: string;
+  project_id?: string | null;
+  keyword?: string | null;
+  priority: TaskPriorityEnumType | null;
+  status?: TaskStatusEnumType | null;
+  assigned_to: string | null;
+  dueDate?: string;
+  page_number: number | null;
+  page_size: number | null;
+};
+
+export type AllTaskResponseType = {
+  message: string;
+  tasks: TaskType[];
+  pagination: PaginationType;
+};
